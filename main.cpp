5a4a62c7fea8a39aa69428d2b1f48bf980151258
tlblanton <tylerblanton635@gmail.com>
2016-01-26 10:05:14 -0700

    initialized with documentation information

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
+/* main.cpp
+ *
+ * This is the main file for c++ version of pa1md5 for Spring 2016 Advanced Programming at the University of Colorado
+ * Denver.
+ *
+ * Author: Tyler Blanton
+ * Date Started: January 26, 2016
+ * Date Due: February 9, 2016
+ * repository url: https://github.com/tlblanton/pa1md5_cpp.git
+ */
+
 #include <iostream>
 
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+int main()
+{
+    cout << "I haven't started yet!" << endl;
 }
